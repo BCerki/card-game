742b92376d6b5b6da64f761880662b77462d4dde
unknown <briannac@shaw.ca>
2021-07-28 20:53:08 +0100

    make card outline and set up colors

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,18 @@
 export default function Card(props) {
-  return <div>{props.card}</div>;
+  const value = props.card.substring(1);
+  const suit = props.card.substring(0, 1);
+
+  const determineColor = function () {
+    if (suit === "♣" || suit === "♠") {
+      return "black-suit";
+    } else {
+      return "red-suit";
+    }
+  };
+  return (
+    <div className={"card-face " + determineColor()}>
+      {value}
+      {suit}
+    </div>
+  );
 }

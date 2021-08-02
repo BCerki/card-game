908dffb6924dd3afa0a87dd0e982915cb9b422c7
unknown <briannac@shaw.ca>
2021-08-02 19:43:48 +0100

    add card back background and make winner announcement more legible

--- src/components/CardBack.js
+++ src/components/CardBack.js
@@ -1,9 +1,17 @@
 export default function Card(props) {
+  // const determineDeck = function () {
+  //   if (props.deck === "COMPUTER") {
+  //     return "computer-deck";
+  //   }
+  //   return "player-deck";
+  // };
   return (
     <div className={"card-back"}>
-      <div className={"deck-label"}>{props.deck} Deck</div>
-      <div className={"cards-remaining"}>
-        {props.cardsRemaining} cards remaining
+      <div className={"card-back-design"}>
+        <div className={"deck-label"}>{props.deck} Deck</div>
+        <div className={"cards-remaining"}>
+          {props.cardsRemaining} cards remaining
+        </div>
       </div>
     </div>
   );
